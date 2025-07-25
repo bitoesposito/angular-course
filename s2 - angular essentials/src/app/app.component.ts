54c334be10f71ac3ee965f251e31e72e611c3670
Vito Esposito <info@vitoesposito.it>
2025-07-25 18:56:07 +0200

    s2e28

--- s2 - angular essentials/src/app/app.component.ts
+++ s2 - angular essentials/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { UserComponent } from './components/user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent,
+    UserComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
