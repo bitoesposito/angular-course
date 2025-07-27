fafe211be6463b2e879e69282463e819ce8774d6
Vito Esposito <info@vitoesposito.it>
2025-07-27 13:45:25 +0200

    s2e37

--- s2 - angular essentials/src/app/app.component.ts
+++ s2 - angular essentials/src/app/app.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { UserComponent } from './components/user/user.component';
+import { DUMMY_USERS } from './components/user/dummy-users';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     HeaderComponent,
-    UserComponent
+    UserComponent,
+    TasksComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+
+  onSelectUserLog(id: string) {
+    console.log('app log: ' + id);
+  } 
+}
--- s2 - angular essentials/src/app/components/user/user.component.ts
+++ s2 - angular essentials/src/app/components/user/user.component.ts
@@ -1,7 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from './dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, computed, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -10,11 +7,17 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => '/assets/users/' + this.selectedUser().avatar);
+  @Input({required: true}) id!: string;
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+  @Output() select = new EventEmitter<string>();
 
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => {
+    return '/assets/users/' + this.avatar;
+  })
+
+  onSelectUserEmit() {
+    console.log('user emit: ' + this.id);
+    this.select.emit(this.id);
   }
 }
