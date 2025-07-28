bebecd49080cb78c91e889ae54a86f8eb8053c82
Vito Esposito <info@vitoesposito.it>
2025-07-28 23:20:36 +0200

    s2e63

---
+++ s2 - angular essentials/src/app/components/tasks/new-task/new-task.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NewTaskData } from '../task/task.model';
+
+@Component({
+  selector: 'app-new-task',
+  standalone: true,
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css'
+})
+export class NewTaskComponent {
+  @Output() close = new EventEmitter<void>();
+  @Output() add = new EventEmitter<NewTaskData>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDueDate = '';
+
+  onClose() {
+    this.close.emit();
+  }
+
+  onSubmit() {
+    this.add.emit({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      dueDate: this.enteredDueDate
+    });
+  }
+}
