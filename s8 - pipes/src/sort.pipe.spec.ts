0065b29847192fdc923987789bb33d35d33705ef
Vito Esposito <info@vitoesposito.it>
2025-08-07 19:06:00 +0200

    s8e172

---
+++ s8 - pipes/src/sort.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { SortPipe } from './app/sort.pipe';
+
+describe('SortPipe', () => {
+  it('create an instance', () => {
+    const pipe = new SortPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
