688d51c37b46a30068284e3566a862f407c3335a
Vito Esposito <info@vitoesposito.it>
2025-08-12 19:52:34 +0200

    s9e190

--- s9 - dependency injection/src/app/tasks/tasks-list/tasks-list.component.ts
+++ s9 - dependency injection/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -2,6 +2,7 @@ import { Component, computed, inject, signal } from '@angular/core';
 
 import { TaskItemComponent } from './task-item/task-item.component';
 import { TasksService } from '../tasks.service';
+import { TASK_STATUS_OPTIONS, taskStatusOptionsProvider } from '../task.model';
 
 @Component({
   selector: 'app-tasks-list',
@@ -9,17 +10,19 @@ import { TasksService } from '../tasks.service';
   templateUrl: './tasks-list.component.html',
   styleUrl: './tasks-list.component.css',
   imports: [TaskItemComponent],
+  providers: [taskStatusOptionsProvider]
 })
 export class TasksListComponent {
   private tasksService = inject(TasksService);
-  selectedFilter = signal<string>('all');
+  private selectedFilter = signal<string>('all');
+  taskStatusOptions = inject(TASK_STATUS_OPTIONS);
   tasks = computed(() => {
     switch (this.selectedFilter()) {
       case 'all':
         return this.tasksService.allTasks();
       case 'open':
         return this.tasksService.allTasks().filter(task => task.status === 'OPEN');
-      case 'inProgress':
+      case 'in-progress':
         return this.tasksService.allTasks().filter(task => task.status === 'IN_PROGRESS');
       case 'done':
         return this.tasksService.allTasks().filter(task => task.status === 'DONE');
