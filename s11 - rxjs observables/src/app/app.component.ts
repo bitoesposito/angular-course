fe3f47751e08717e88d99e49788764c871fb53a6
Vito Esposito <info@vitoesposito.it>
2025-08-14 18:11:46 +0200

    s10e210

---
+++ s11 - rxjs observables/src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component, computed, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
+import { COMPOSITION_BUFFER_MODE } from '@angular/forms';
+import { interval, map } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html'
+})
+export class AppComponent implements OnInit {
+
+  clickCount = signal(0)
+  interval = signal(0)
+  doubleInterval = computed(() => this.interval() * 2)
+  private destroyRef = inject(DestroyRef)
+
+  constructor() {
+    effect(() => {
+      console.log(`clicked button ${this.clickCount()} times`)
+    })
+  }
+
+  ngOnInit(): void {
+
+    setInterval(() => {
+      this.interval.update(prevIntervalNumber => prevIntervalNumber + 1)
+    }, 1000)
+    
+    // const subscription = interval(1000).pipe(
+    //   map((val) => val * 2 )
+    // ).subscribe({
+    //   next: (val) => console.log(val)
+    // })
+
+    // this.destroyRef.onDestroy(() => {
+    //   subscription.unsubscribe()
+    // })
+  }
+
+  onClick() {
+    this.clickCount.update(prevCount => prevCount + 1)
+  }
+}
