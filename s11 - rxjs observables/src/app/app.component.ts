3b2b54f0ca9c5dade56389b180f6e5310caef3a7
Vito Esposito <info@vitoesposito.it>
2025-08-18 20:29:20 +0200

    s12e215

--- s11 - rxjs observables/src/app/app.component.ts
+++ s11 - rxjs observables/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, computed, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { COMPOSITION_BUFFER_MODE } from '@angular/forms';
-import { interval, map } from 'rxjs';
+import { interval, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,31 +11,57 @@ import { interval, map } from 'rxjs';
 export class AppComponent implements OnInit {
 
   clickCount = signal(0)
-  interval = signal(0)
-  doubleInterval = computed(() => this.interval() * 2)
+  clickCount$ = toObservable(this.clickCount)
+  interval$ = interval(1000)
+  intervalSignal = toSignal(this.interval$, { initialValue: 0 })
+  // interval = signal(0)
+  // doubleInterval = computed(() => this.interval() * 2)
+  customInterval$ = new Observable((subscriber) => {
+    let timesExecuted = 0
+    setInterval(() => {
+      // subscriber.error(new Error('Error'))
+      if (timesExecuted < 5) {
+        console.log('Emitting new value...');
+        subscriber.next({message: `New value ${timesExecuted++}`})
+      } else {
+        subscriber.complete()
+      }
+    }, 100)
+  })
+  customIntervalSignal = toSignal(this.customInterval$, { initialValue: 0 })
   private destroyRef = inject(DestroyRef)
 
   constructor() {
-    effect(() => {
-      console.log(`clicked button ${this.clickCount()} times`)
-    })
+    // effect(() => {
+    //   console.log(`clicked button ${this.clickCount()} times`)
+    // })
   }
 
   ngOnInit(): void {
 
-    setInterval(() => {
-      this.interval.update(prevIntervalNumber => prevIntervalNumber + 1)
-    }, 1000)
-    
+    // setInterval(() => {
+    //   this.interval.update(prevIntervalNumber => prevIntervalNumber + 1)
+    // }, 1000)
+
     // const subscription = interval(1000).pipe(
     //   map((val) => val * 2 )
     // ).subscribe({
     //   next: (val) => console.log(val)
     // })
 
-    // this.destroyRef.onDestroy(() => {
-    //   subscription.unsubscribe()
-    // })
+    this.customInterval$.subscribe({
+      next: (val) => console.log(val),
+      complete: () => console.log('COMPLETED!'),
+      error: (err) => console.log(err)
+    })
+    
+    const subscription = this.clickCount$.subscribe({
+      next: (val) => console.log(`Clicked button ${this.clickCount()} times`)
+    })
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe()
+    })
   }
 
   onClick() {
